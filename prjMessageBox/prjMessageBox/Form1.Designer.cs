a17fb2c585951e10ddbb1f68a094568141c4e3f1
raizengxd <rickdavidsangalang@gmail.com>
2020-05-14 07:24:30 +0800

    add

---
+++ prjMessageBox/prjMessageBox/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace prjMessageBox
+{
+    partial class frmMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(80, 109);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(111, 40);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "&Click Me..";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // frmMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnClickMe);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMessageBox";
+            this.Text = "Message Box";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+    }
+}
+
---
+++ prjMessageBox/prjMessageBox/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjMessageBox
+{
+    public partial class frmMessageBox : Form
+    {
+        private Random rnd = new Random();
+
+
+        public frmMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            string message = "Do you want to abort this operation?";
+            string title = "Close Window";
+            MessageBoxButtons buttons = MessageBoxButtons.AbortRetryIgnore;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+            if (result == DialogResult.Abort)
+            {
+                this.Close();
+            }
+            if (result == DialogResult.Retry)
+            {
+                // Do nothing
+            }
+            else
+            {
+                Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+
+                BackColor = randomColor;
+            } 
+        }
+    }
+}
