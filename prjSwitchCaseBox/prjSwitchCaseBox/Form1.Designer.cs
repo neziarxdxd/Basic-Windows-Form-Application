8b5e39433922c944e55c599fd63c496bf620a187
raizengxd <rickdavidsangalang@gmail.com>
2020-05-14 10:24:56 +0800

    add

---
+++ prjSwitchCaseBox/prjSwitchCaseBox/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace prjSwitchCaseBox
+{
+    partial class frmSwitchCaseBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtShadow = new System.Windows.Forms.TextBox();
+            this.lblText = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtShadow
+            // 
+            this.txtShadow.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtShadow.Location = new System.Drawing.Point(179, 47);
+            this.txtShadow.Name = "txtShadow";
+            this.txtShadow.Size = new System.Drawing.Size(178, 20);
+            this.txtShadow.TabIndex = 0;
+            this.txtShadow.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(55, 50);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(100, 13);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "Input Text Here:";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(58, 125);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(97, 36);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "&Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(188, 125);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(97, 36);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(314, 125);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 36);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmSwitchCaseBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(479, 205);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.txtShadow);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmSwitchCaseBox";
+            this.Text = "Message Box";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtShadow;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ prjSwitchCaseBox/prjSwitchCaseBox/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjSwitchCaseBox
+{
+    public partial class frmSwitchCaseBox : Form
+    {
+        public frmSwitchCaseBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string letter = txtShadow.Text;
+                if (Char.IsDigit(letter, 0))
+                {
+                    DialogResult result = MessageBox.Show("It's a Number?", "Close Window", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        this.Close();
+                    }
+                    else
+                    {
+                        // Do Nothing
+                    }
+                }
+
+
+                else if (Char.IsLetter(letter, 0))
+                {
+                    DialogResult result1 = MessageBox.Show("It's a Letter?", "Close Window", MessageBoxButtons.YesNo);
+                    if (result1 == DialogResult.Yes)
+                    {
+                        this.Close();
+                    }
+                    else
+                    {
+                        // Do Nothing
+                    }
+                }
+                else
+                {
+                    DialogResult result2 = MessageBox.Show("It's Something Else?", "Close Window", MessageBoxButtons.YesNo);
+                    if (result2 == DialogResult.Yes)
+                    {
+                        this.Close();
+                    }
+                    else
+                    {
+                        // Do Nothing
+                    }
+                }
+
+            }
+            catch (Exception exc)
+            {
+                DialogResult result2 = MessageBox.Show("It's Something Else?", "Close Window", MessageBoxButtons.YesNo);
+                if (result2 == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+                else
+                {
+                    // Do Nothing
+                }
+            }
+        }
+        
+       
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            txtShadow.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+    
